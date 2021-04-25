ffa935ab35daa4c345e32aa065fbdd07f3f3944f
George Grainger <GraingerGY@cardiff.ac.uk>
2021-04-25 20:52:08 +0100

    Add fix for OpenShift deployment

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/home')
@@ -7,6 +9,8 @@ def home():
     return render_template('home.html')
 
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+# For openshift deployment to work, app needs to be called application
+application = app
